5a8b01ae381e613fe696bcf85e5cbf641021fc2d
Baki Goxhaj <banago@gmail.com>
2023-01-18 13:53:41 +0100

    Install WP

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -393,7 +393,7 @@ class SiteController extends Controller
 
         // return response()->json([]);
         return response()->json([
-            'user'              => $user->username,
+            'user'              => $site->username,
             'domain'            => $site->domain.'/wp-admin/',
             'username'          => $wp['user'],
             'password'          => $wp['pass'],
